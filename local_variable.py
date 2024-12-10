152b88fd5754ab2d1005dfddac4ee8b7b56aabcd
Python <58145537+aiella-p@users.noreply.github.com>
2024-12-10 08:46:20 +0000

    Delete local_variable.py

--- local_variable.py
+++
@@ -1,9 +0,0 @@
-# python-local-variable.py
-# 
-def function_local(a):
-	print('a is -> ',a)
-	a = 50
-	print('After new value within the function a is -> ',a)
-a = 100
-function_local(40)
-print('Value of a is ->',a)
