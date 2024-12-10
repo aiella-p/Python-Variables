9b9b259d7ea910be7f7f5302511928092050f01b
Python <58145537+aiella-p@users.noreply.github.com>
2024-12-10 08:46:52 +0000

    Delete nonlocal.py

--- nonlocal.py
+++
@@ -1,9 +0,0 @@
-def outside():
-       a = 10
-       def inside():
-              nonlocal a
-              a = 20
-              print("The value of a in inside() function - ", a)
-       inside()
-       print("The value of a in outside() function -  ", a)
-outside()
